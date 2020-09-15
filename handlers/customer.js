59f69f8de9a991e552350a38b2254ba8e724e46a
Jerry Oates <jerryoates1@gmail.com>
2020-09-15 12:08:43 -0400

    added first customer lambdas

--- handlers/customer.js
+++ handlers/customer.js
@@ -26,3 +26,70 @@ module.exports.getCustomersByCurrentUser = async (event, context) => {
         }
     }
 }
+
+module.exports.getCustomersByTeam = async (event, context) => {
+
+    try {
+
+        const user = await getCurrentUser(event.headers.Authorization)
+
+        const customers = await Customer.findAll({
+            where: {
+                teamId: user.teamId
+            }
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(customers)
+        }
+
+    } catch (err) {
+
+        return {
+            statusCode: 401
+        }
+    }
+}
+
+module.exports.getCustomer = async (event, context) => {
+
+    try {
+
+        const user = await getCurrentUser(event.headers.Authorization)
+
+        const customerId = event.pathParameters.customerId
+
+        const results = await Customer.findOne({
+            where: {
+                id: customerId,
+                userId: user.id
+            }
+        })
+
+        if (results != null) {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(results)
+            }
+        } else {
+            return {
+                statusCode: 404
+            }
+        }
+
+    } catch (err) {
+
+        return {
+            statusCode: 401
+        }
+
+    }
+
+}
+module.exports.getLocationsByCustomer = async (event, context) => {
+
+}
+module.exports.getCustomerById = async (event, context) => {
+
+}
