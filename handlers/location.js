031057b0c857f1b18ea8be6694b0b92ddd19a072
Jerry Oates <jerryoates1@gmail.com>
2020-11-03 15:43:12 -0500

    lambda and endpoint

---
+++ handlers/location.js
@@ -0,0 +1,67 @@
+'use strict';
+const getCurrentUser = require('.././helpers/user').getCurrentUser
+const { Customer, CustomerLocation, Lane, LanePartner, CustomerLane, User, Team } = require('.././models');
+
+module.exports.getCustomerLocation = async (event, context) => {
+
+    try {
+
+        const user = await getCurrentUser(event.headers.Authorization)
+
+        if (user.id == undefined) {
+
+            return {
+                statusCode: 401
+            }
+        }
+
+        const customerLocationId = event.pathParameters.id
+
+        const results = await CustomerLocation.findOne({
+            where: {
+                id: customerLocationId
+            },
+            include: [{
+                model: CustomerLane,
+                include: [{
+                    model: LanePartner,
+                    required: true
+                }]
+            }, {
+                model: Customer,
+                required: true,
+                attributes: ['teamId'],
+                include: [{
+                    model: Team,
+                    required: true,
+                    attributes: ['brokerageId'],
+                    where: {
+                        brokerageId: user.brokerageId
+                    }
+                }]
+            }]
+        })
+
+        console.log(results.toJSON())
+
+        if (results != null) {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(results)
+            }
+        } else {
+            return {
+                statusCode: 404
+            }
+        }
+    }
+    catch (err) {
+
+        console.log(err)
+
+        return {
+            statusCode: 500
+        }
+    }
+
+};
