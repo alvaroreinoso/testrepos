807151b8485e9418acffce1571a95487df91db29
Jerry Oates <jerryoates1@gmail.com>
2020-11-05 13:56:15 -0500

    count number of days

--- demo.js
+++ demo.js
@@ -1,3 +1,4 @@
+const dateFns = require('date-fns')
 const { Customer, Load, CustomerContact, CustomerLocation, CustomerLane, Team, LanePartner, User } = require('./models')
 
 async function run() {
@@ -18,13 +19,41 @@ async function run() {
 
     const dates = loads.map(load => load.dropDate)
 
+    const firstDate = dateFns.parseISO(dates[0])
+
+    const lastDate = dateFns.parseISO(dates[dates.length - 1])
+
+    const daysBetween = await dateFns.differenceInDays(firstDate, lastDate)
+
+    if (daysBetween == 0) {
+
+        let frequency = dates.length
+
+        return frequency
+    } else {
+
+        const numberOfWeeks = daysBetween / 7
+
+        console.log(Math.round(numberOfWeeks))
+    }
+
     const count = dates.length
+
+    console.log(daysBetween)
+
+    dates.forEach(date => {
+        dateFns.parseISO(date)
+    })
+
+    // get first and last, count number of days between, divide by 7
+
+
 
 
 
     // create array of week objects 
 
-    console.log(frequency)
+    // console.log(frequency)
 
     console.log(dates)
     // console.log(cLane.toJSON())
