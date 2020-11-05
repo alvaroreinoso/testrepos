cefd6e5a7668b8a7bf318d2583ecb77b1c5cc70e
Jerry Oates <jerryoates1@gmail.com>
2020-11-05 14:12:43 -0500

    added ledger to customer locations

--- migrations/20200831213717-create-customer-location.js
+++ migrations/20200831213717-create-customer-location.js
@@ -22,6 +22,10 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: true
       },
+      ledgerId: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
       address: {
         type: Sequelize.STRING,
         allowNull: false
