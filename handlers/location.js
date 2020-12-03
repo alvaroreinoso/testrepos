ad8bc8bb58a7cb8d49f81d3644d9e08212ad2e8e
Jerry Oates <jerryoates1@gmail.com>
2020-12-03 15:34:16 -0500

    get, add, delete teammates from location

--- handlers/location.js
+++ handlers/location.js
@@ -1,6 +1,6 @@
 'use strict';
 const getCurrentUser = require('.././helpers/user').getCurrentUser
-const { Customer, CustomerLocation, Lane, LanePartner, Location, User, Team } = require('.././models');
+const { Customer, CustomerLocation, Lane, LanePartner, Location, TaggedLocation } = require('.././models');
 const { Op } = require("sequelize");
 
 module.exports.getLocationById = async (event, context) => {
@@ -111,4 +111,108 @@ module.exports.getLanesForLocation = async (event, context) => {
         }
     }
 
-} 
+}
+
+module.exports.getTeammatesForLocation = async (event, context) => {
+
+    try {
+
+        const user = await getCurrentUser(event.headers.Authorization)
+
+
+        if (user.id == null) {
+            return {
+                statusCode: 401
+            }
+        }
+
+        const locationId = event.pathParameters.locationId
+
+        const location = await Location.findOne({
+            where: {
+                id: locationId
+            },
+        })
+
+        const users = await location.getUsers()
+
+        return {
+            body: JSON.stringify(users),
+            statusCode: 200
+        }
+    }
+    catch (err) {
+
+        console.log(err)
+        return {
+            statusCode: 500
+        }
+    }
+
+
+}
+
+module.exports.addTeammateToLocation = async (event, context) => {
+
+    try {
+        const user = await getCurrentUser(event.headers.Authorization)
+
+        if (user.id == null) {
+            return {
+                statusCode: 401
+            }
+        }
+
+        const request = JSON.parse(event.body)
+
+        const locationId = request.locationId
+        const userId = request.userId
+
+        await TaggedLocation.findOrCreate({
+            where: {
+                locationId: locationId,
+                userId: userId
+            }
+        })
+
+        return {
+            statusCode: 204
+        }
+    }
+    catch (err) {
+        return {
+            statusCode: 500
+        }
+    }
+}
+
+module.exports.deleteTeammateFromLocation = async (event, context) => {
+
+    try {
+        const user = await getCurrentUser(event.headers.Authorization)
+
+        if (user.id == null) {
+            return {
+                statusCode: 401
+            }
+        }
+
+        const request = JSON.parse(event.body)
+
+        await TaggedLocation.destroy({
+            where: {
+                userId: request.userId,
+                locationId: request.locationId
+            }
+        })
+
+        return {
+            statusCode: 204
+        }
+    }
+    catch (err) {
+        return {
+            statusCode: 500
+        }
+    }
+}
