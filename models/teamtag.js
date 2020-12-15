55b7ce6432976da251bbbffdfeaee4777abf39af
Jerry Oates <jerryoates1@gmail.com>
2020-12-15 14:22:13 -0500

    create models and migrations

---
+++ models/teamtag.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class TeamTag extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  TeamTag.init({
+    teamId: DataTypes.INTEGER,
+    tagId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'TeamTag',
+  });
+  return TeamTag;
+};
