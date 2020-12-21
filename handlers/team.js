5557d5acd4685854404ca09f68d4311b27c0c5a7
Jerry Oates <jerryoates1@gmail.com>
2020-12-21 12:09:33 -0600

    remove get lanes for team

--- handlers/team.js
+++ handlers/team.js
@@ -6,7 +6,6 @@ const { Op } = require("sequelize");
 
 module.exports.getTeamById = async (event, context) => {
 
-
     const user = await getCurrentUser(event.headers.Authorization)
 
     if (user.id == null) {
@@ -132,94 +131,6 @@ module.exports.getTeamById = async (event, context) => {
         }
     } catch (err) {
         console.log(err)
-        return {
-            statusCode: 500
-        }
-    }
-
-}
-module.exports.getLanesForTeam = async (event, context) => {
-
-    const user = await getCurrentUser(event.headers.Authorization)
-
-    if (user.id == null) {
-        return {
-            statusCode: 401
-        }
-    }
-
-    try {
-        const teamId = event.pathParameters.teamId
-
-        const team = await Team.findOne({
-            where: {
-                id: teamId,
-                brokerageId: user.brokerageId
-            }
-        })
-
-        const customers = await team.getCustomers()
-
-        const customerLocations = await customers.map(async customer => {
-            const cLs = await customer.getCustomerLocations({
-                include: [{
-                    model: Location,
-                    required: true
-                }]
-            })
-
-            return cLs
-        })
-
-        const clResolved = await Promise.all(customerLocations)
-
-        let locations = []
-        for (const arr of clResolved) {
-            for (const loc of arr) {
-                locations.push(loc.Location)
-            }
-        }
-
-        let lanes = []
-        for (const loc of locations) {
-            const lanesForLocation = await loc.getLanes({
-                include: [{
-                    model: Location,
-                    as: 'origin',
-                    include: [{
-                        model: CustomerLocation,
-                        include: [{
-                            model: Customer
-                        }]
-                    }, {
-                        model: LanePartner
-                    }]
-                },
-                {
-                    model: Location,
-                    as: 'destination',
-                    include: [{
-                        model: CustomerLocation,
-                        include: [{
-                            model: Customer
-                        }]
-                    }, {
-                        model: LanePartner
-                    }]
-                }]
-            })
-
-            for (const lane of lanesForLocation) {
-                lanes.push(lane)
-            }
-        }
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify(lanes)
-        }
-
-    } catch (err) {
         return {
             statusCode: 500
         }
