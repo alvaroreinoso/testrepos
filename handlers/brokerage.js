cb9ca9b6e9a2fb6ccd640b63ac6df3c681fa95a9
Jerry Oates <jerryoates1@gmail.com>
2020-12-30 13:10:34 -0500

    getBrokerage with top teams and teammates

--- handlers/brokerage.js
+++ handlers/brokerage.js
@@ -1,16 +1,98 @@
 'use strict';
 const getCurrentUser = require('.././helpers/user').getCurrentUser
-const { Customer, Brokerage, CustomerLocation, LanePartner, Contact, LaneContact, Location, Lane } = require('.././models')
+const { Customer, Brokerage, CustomerLocation, LanePartner, Team, User, Location, Lane } = require('.././models')
 const { Op } = require("sequelize");
 
 module.exports.getBrokerage = async (event, context) => {
 
     const user = await getCurrentUser(event.headers.Authorization)
+    const brokerageId = event.pathParameters.brokerageId
 
     if (user.id == null) {
         return {
             statusCode: 401
         }
+    }
+
+    const brokerage = await Brokerage.findOne({
+        where: {
+            id: brokerageId,
+        },
+    })
+
+    const customers = await brokerage.getCustomers()
+    const customerCount = customers.length
+
+    const teammates = await brokerage.getUsers()
+    const teams = await brokerage.getTeams()
+
+    let teammatesWithCount = []
+
+    for (const team of teams) {
+
+        let revenuePerUser = []
+        let lanesForUser = new Set()
+
+        const users = await team.getUsers()
+        for (const mate of users) {
+
+            const customers = await mate.getCustomers()
+
+            mate.dataValues.customerCount = customers.length
+
+            const lanes = await mate.getLanes()
+
+            for (const lane of lanes) {
+
+                lanesForUser.add(lane.id)
+            }
+
+            if (lanes.length != 0) {
+
+                const laneSpend = await lanes.map(lane => lane.spend)
+                const revenue = await laneSpend.reduce((a, b) => (a + b))
+                mate.dataValues.revenue = revenue
+                revenuePerUser.push(revenue)
+            }
+            else {
+                mate.dataValues.revenue = 0
+            }
+
+            teammatesWithCount.push(mate)
+        }
+
+    
+
+        const lanesForTeam =  [...lanesForUser].map(async laneId => {
+
+            const lane = await Lane.findOne({
+                where: {
+                    id: laneId
+                },
+            })
+
+            return lane
+        })
+
+        const lanesResolved = await Promise.all(lanesForTeam)
+        const laneSpend = lanesResolved.map(lane => lane.spend)
+        const teamSpend = await laneSpend.reduce((a, b) => a + b)
+
+        team.dataValues.revenue = teamSpend
+    }
+
+    const topTeams = teams.sort((a, b) => { return b.dataValues.revenue - a.dataValues.revenue })
+    const topTeammates = teammatesWithCount.sort((a, b) => { return b.dataValues.revenue - a.dataValues.revenue })
+    const teammateCount = teammates.length
+
+    brokerage.dataValues.customers = customerCount
+    brokerage.dataValues.teammates = teammateCount
+    brokerage.dataValues.topTeammates = topTeammates
+    brokerage.dataValues.topTeams = topTeams
+
+    return {
+        body: JSON.stringify(brokerage),
+        statusCode: 200
     }
 }
 
@@ -33,21 +115,21 @@ module.exports.getLanesForBrokerage = async (event, context) => {
 
     try {
 
-    const locations = await Location.findAll({
-        include: [{
-            model: CustomerLocation,
-            required: true,
+        const locations = await Location.findAll({
             include: [{
-                model: Customer,
+                model: CustomerLocation,
                 required: true,
-                where: {
-                    brokerageId: brokerageId
-                }
+                include: [{
+                    model: Customer,
+                    required: true,
+                    where: {
+                        brokerageId: brokerageId
+                    }
+                }]
             }]
-        }]
-    })
+        })
 
-    let lanes = []
+        let lanes = []
         for (const location of locations) {
             const locationLanes = await Lane.findAll({
                 where: {
@@ -94,20 +176,20 @@ module.exports.getLanesForBrokerage = async (event, context) => {
         }
 
         const laneSpend = lanes.map(lane => lane.spend)
-        const teamSpend = await laneSpend.reduce((a, b) => a + b)
+        const brokerageSpend = await laneSpend.reduce((a, b) => a + b)
 
         const loadsPerWeek = await lanes.reduce((a, b) => ({ frequency: a.frequency + b.frequency }))
 
         const body = {
-            revenue: teamSpend,
+            revenue: brokerageSpend,
             loadsPerWeek: loadsPerWeek.frequency,
             Lanes: lanes
         }
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(body)
-    }
+        return {
+            statusCode: 200,
+            body: JSON.stringify(body)
+        }
     } catch (err) {
 
         return {
