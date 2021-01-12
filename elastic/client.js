3e9e43602952712bcd0dcc25221ea088ca5f398b
Jerry Oates <jerryoates1@gmail.com>
2021-01-12 16:04:37 -0500

    setup for serverless ci/cd

--- elastic/client.js
+++ elastic/client.js
@@ -2,7 +2,12 @@ const elasticsearch = require('elasticsearch');
 require('dotenv').config()
 
 const client = new elasticsearch.Client({
-    host: process.env.SEARCH_URL,
+    // host: process.env.SEARCH_URL,
+    node: process.env.SEARCH_URL,
+    auth: {
+        username: process.env.SEARCH_USER,
+        password: process.env.SEARCH_PASSWORD
+    },
     log: [{
         type: 'stdio',
         levels: ['error']
