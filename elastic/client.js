2f5fe5f54e155a660538ffdcb67fbb5574a7c393
Jerry Oates <jerryoates1@gmail.com>
2021-01-25 15:05:06 -0500

    fix elasticsearch config

--- elastic/client.js
+++ elastic/client.js
@@ -2,20 +2,12 @@ const elasticsearch = require('@elastic/elasticsearch');
 require('dotenv').config()
 
 const client = new elasticsearch.Client({
-    // connectionClass: AWSConnector,
-    // node: process.env.SEARCH_URL,
     node: process.env.SEARCH_URL,
-    // node: `${process.env.SEARCH_USER}:${process.env.SEARCH_PASSWORD}@${process.env.SEARCH_URL}`,
-    // node: `https://${process.env.SEARCH_USER}:${process.env.SEARCH_PASSWORD}@search-staging-search-4xcqqvyidrf22wjiokahpzkwvy.us-east-1.es.amazonaws.com`,
-    // auth: {
-    //     username: process.env.SEARCH_USER,
-    //     password: process.env.SEARCH_PASSWORD
-    // },
     log: [{
         type: 'stdio',
         levels: ['error']
     }],
-    // apiVersion: '7.7'
+    apiVersion: '7.10'
 });
 
 module.exports = client;
