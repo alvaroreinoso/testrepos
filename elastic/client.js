28f927971605af65c5ab003a7fd719719469d449
Jerry Oates <jerryoates1@gmail.com>
2021-01-25 12:05:31 -0500

    elastic vpc connection

--- elastic/client.js
+++ elastic/client.js
@@ -8,10 +8,10 @@ const client = new elasticsearch.Client({
     // host: process.env.SEARCH_URL,
     // node: `${process.env.SEARCH_USER}:${process.env.SEARCH_PASSWORD}@${process.env.SEARCH_URL}`,
     // node: `https://${process.env.SEARCH_USER}:${process.env.SEARCH_PASSWORD}@search-staging-search-4xcqqvyidrf22wjiokahpzkwvy.us-east-1.es.amazonaws.com`,
-    auth: {
-        username: process.env.SEARCH_USER,
-        password: process.env.SEARCH_PASSWORD
-    },
+    // auth: {
+    //     username: process.env.SEARCH_USER,
+    //     password: process.env.SEARCH_PASSWORD
+    // },
     log: [{
         type: 'stdio',
         levels: ['error']
