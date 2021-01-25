5dab6cb2af998fa73805fe9d7b933b3797de5f69
Jerry Oates <jerryoates1@gmail.com>
2021-01-25 15:33:15 -0500

    script for elastic index only setup

---
+++ elastic/indices.js
@@ -0,0 +1,38 @@
+const client = require('./client')
+
+async function init() {
+
+    await client.indices.create({
+        index: 'customer',
+    })
+
+    await client.indices.create({
+        index: 'message',
+    })
+
+    await client.indices.create({
+        index: 'lane',
+    })
+
+    await client.indices.create({
+        index: 'team',
+    })
+
+    await client.indices.create({
+        index: 'lane_partner',
+    })
+
+    await client.indices.create({
+        index: 'customer_location',
+    })
+
+    await client.indices.create({
+        index: 'user',
+    })
+
+    await client.indices.create({
+        index: 'contact',
+    })
+}
+
+init()
