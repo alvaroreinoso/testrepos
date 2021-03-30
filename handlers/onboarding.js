095f6ce126e85574028a134cc619a35f637b5eca
Jerry Oates <jerryoates1@gmail.com>
2021-03-30 16:39:47 -0400

    additional info on onboarding customers response

--- handlers/onboarding.js
+++ handlers/onboarding.js
@@ -3,8 +3,9 @@ const getCurrentUser = require('.././helpers/user')
 const sendRequestAccountEmail = require('../ses/templates/requestAccount')
 const sendCreateAccountEmail = require('../ses/templates/createAccount')
 const testInvite = require('../ses/templates/testInvite')
-const { Team, User, Customer, Brokerage, Ledger } = require('.././models');
+const { Team, User, Customer, Lane, Brokerage, Ledger, Location } = require('.././models');
 const corsHeaders = require('.././helpers/cors')
+const { Op } = require("sequelize");
 const { v4: uuidv4 } = require('uuid');
 
 module.exports.requestAccount = async (event, context) => {
@@ -42,18 +43,18 @@ module.exports.requestAccount = async (event, context) => {
                 const brokerage = await Brokerage.create({
                     pin: uuid,
                 })
-    
+
                 const ledger = await Ledger.create({
                     brokerageId: brokerage.id
                 })
-    
+
                 brokerage.ledgerId = ledger.id
                 await brokerage.save()
-    
+
                 const userLedger = await Ledger.create({
                     brokerageId: brokerage.id
                 })
-    
+
                 const user = await User.create({
                     firstName: request.firstName,
                     lastName: request.lastName,
@@ -207,18 +208,79 @@ module.exports.getCustomersForBrokerage = async (event, context) => {
             }
         })
 
+        const customersWithStats = await Promise.all(await customers.map(async customer => {
+
+            const locations = await customer.getCustomerLocations({
+                include: [{
+                    model: Location,
+                }]
+            })
+
+            let laneIds = new Set()
+
+            for (const location of locations) {
+                const locationLanes = await Lane.findAll({
+                    where: {
+                        [Op.or]: [
+                            { originLocationId: location.Location.id },
+                            { destinationLocationId: location.Location.id }
+                        ]
+                    },
+                    order: [
+                        ['frequency', 'DESC'],
+                    ],
+                })
+
+                for (const lane of locationLanes) {
+                    laneIds.add(lane.id)
+                }
+            }
+
+            const lanes = await Promise.all([...laneIds].map(async laneId => {
+
+                const lane = await Lane.findOne({
+                    where: {
+                        id: laneId
+                    }
+                })
+
+                return lane
+            }))
+
+            customer.dataValues.laneCount = lanes.length
+            customer.dataValues.locationCount = locations.length
+
+            if (lanes.length == 0) {
+                customer.dataValues.loadsPerMonth = 0
+                customer.dataValues.spendPerMonth = 0
+
+                return customer
+
+            } else {
+
+                const spendPerMonth = await lanes.reduce((a, b) => ({ spend: a.spend + b.spend }))
+                const loadsPerWeek = await lanes.reduce((a, b) => ({ frequency: a.frequency + b.frequency }))
+                const loadsPerMonth = loadsPerWeek.frequency * 4
+
+                customer.dataValues.loadsPerMonth = loadsPerMonth
+                customer.dataValues.spendPerMonth = spendPerMonth.spend
+
+                return customer
+            }
+        }))
+
         return {
-            body: JSON.stringify(customers),
+            body: JSON.stringify(customersWithStats),
             headers: corsHeaders,
             statusCode: 200
         }
 
     } catch (err) {
 
+        console.log(err)
         return {
             headers: corsHeaders,
             statusCode: 500
         }
     }
-
 }
