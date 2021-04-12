652eba4aa6855e993d8d578a744f55561aa73c8f
Jerry Oates <jerryoates1@gmail.com>
2021-04-12 16:12:35 -0400

    hard code secuirty group

---
+++ s_addTeammatetoLane.js
@@ -0,0 +1,29 @@
+
+var serverlessSDK = require('./serverless_sdk/index.js');
+serverlessSDK = new serverlessSDK({
+  orgId: 'terralanes',
+  applicationName: 'terralanes-backend',
+  appUid: 'MGTDBb0H5JGR9c03rN',
+  orgUid: 'c40ff449-c91a-4aca-9c7e-aa71837ea9d1',
+  deploymentUid: '4d6b95ef-9800-427d-a61e-daee0c3aacb4',
+  serviceName: 'terralanes-api',
+  shouldLogMeta: true,
+  shouldCompressLogs: true,
+  disableAwsSpans: false,
+  disableHttpSpans: false,
+  stageName: 'test-warmup',
+  serverlessPlatformStage: 'prod',
+  devModeEnabled: false,
+  accessKey: null,
+  pluginVersion: '3.7.1',
+  disableFrameworksInstrumentation: false
+});
+
+const handlerWrapperArgs = { functionName: 'terralanes-api-test-warmup-addTeammatetoLane', timeout: 6 };
+
+try {
+  const userHandler = require('././handlers/lane.js');
+  module.exports.handler = serverlessSDK.handler(userHandler.addTeammateToLane, handlerWrapperArgs);
+} catch (error) {
+  module.exports.handler = serverlessSDK.handler(() => { throw error }, handlerWrapperArgs);
+}
