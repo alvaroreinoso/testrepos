d6f5fb48b4dc0867687b4bc5b1f9c4db3dbfca72
Jerry Oates <jerryoates1@gmail.com>
2021-04-21 12:39:30 -0400

    move update brokerage ledgerId column to seed

--- seeders/20200831181559-team.js
+++ seeders/20200831181559-team.js
@@ -5,6 +5,17 @@ const teams = require("../data/teams");
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Ledgers', ledgers, {});
+
+    await queryInterface.changeColumn('Brokerages', 'ledgerId', {
+      type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'Ledgers',
+          key: 'id',
+          as: 'ledgerId'
+        }
+    })
+
     await queryInterface.bulkInsert('Teams', teams, {});
   },
 
