ceb1573afedaa143c5a5e409ccbb4cc80fd82672
Jerry Oates <jerryoates1@gmail.com>
2021-05-03 17:45:20 -0400

    carrier seed data and migration

---
+++ seeders/20210503211416-carriers.js
@@ -0,0 +1,13 @@
+'use strict';
+const carriers = require('../data/carriers')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+   await queryInterface.bulkInsert('Carriers', carriers)
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    
+     await queryInterface.bulkDelete('Carriers', null, {});
+  }
+};
