138b35d18ac52899ff536113948d44386350fee2
Jerry Oates <jerryoates1@gmail.com>
2021-05-14 14:09:31 -0400

    add user search to switch

--- handlers/search.js
+++ handlers/search.js
@@ -137,176 +137,58 @@ module.exports.search = async (event, context) => {
                     headers: corsHeaders,
                     statusCode: 200
                 }
+            } case '/api/search/users': {
 
-            }
-        }
+                const brokerageId = user.brokerageId
 
-    } catch (err) {
-        console.log(err)
-        return {
-            headers: corsHeaders,
-            statusCode: 500
-        }
-    }
-}
+                const query = event.queryStringParameters.q
 
-module.exports.searchLedger = async (event, context) => {
-
-    if (event.source === 'serverless-plugin-warmup') {
-        console.log('WarmUp - Lambda is warm!');
-        return 'Lambda is warm!';
-    }
-
-    const user = await getCurrentUser(event.headers.Authorization)
-
-    const ledgerId = event.queryStringParameters.id
-
-    const ledger = await Ledger.findOne({
-        where: {
-            id: ledgerId,
-            brokerageId: user.brokerageId
-        }
-    })
-
-    if (ledger == null) {
-        return {
-            headers: corsHeaders,
-            statusCode: 401
-        }
-    }
-
-    const query = event.queryStringParameters.q
-
-    const searchResults = await client.search({
-        index: 'message',
-        body: {
-            query: {
-                bool: {
-                    must: [
-                        {
-                            multi_match: {
-                                query: query,
-                                type: "phrase_prefix",
-                                fields: ["content", "userFirstName", "userLastName"]
+                const searchResults = await client.search({
+                    index: 'user',
+                    body: {
+                        query: {
+                            bool: {
+                                must: [
+                                    {
+                                        multi_match: {
+                                            query: query,
+                                            type: "phrase_prefix",
+                                            fields: ["firstName", "lastName", "fullName"]
+                                        }
+                                    },
+                                ],
+                                filter: [
+                                    { "term": { "brokerageId": brokerageId } }
+                                ]
                             }
-                        },
-                    ],
-                    filter: [
-                        { "term": { "ledgerId": ledgerId } }
-                    ]
-                }
-            }
-        }
-    })
-
-    console.log(searchResults.body.hits.hits)
-
-    const dbResults = searchResults.body.hits.hits.map(message => {
-
-        const results = Message.findOne({
-            where: {
-                id: message._source.id
-            },
-            include: {
-                model: User,
-                attributes: ['id', 'firstName', 'lastName', 'profileImage', 'teamId', 'title'],
-                include: {
-                    model: Team
-                }
-            }
-
-        })
-
-        return results
-    })
-
-    const response = await Promise.all(dbResults)
-
-    function compare(a, b) {
-        // if (a.createdAt < b.createdAt) return -1;
-        // if (b.createdAt > a.createdAt) return 1;
-
-        // return 0;
-    }
-
-    const sortedResults = await response.sort(compare)
-
-    return {
-        body: JSON.stringify(sortedResults),
-        headers: corsHeaders,
-        statusCode: 200
-    }
-}
-
-module.exports.searchUsersInBrokerage = async (event, context) => {
-
-    if (event.source === 'serverless-plugin-warmup') {
-        console.log('WarmUp - Lambda is warm!');
-        return 'Lambda is warm!';
-    }
-
-    try {
-
-        const user = await getCurrentUser(event.headers.Authorization)
-
-        if (user.id == null) {
-
-            return {
-                headers: corsHeaders,
-                statusCode: 401
-            }
-        }
-
-        const brokerageId = user.brokerageId
-
-        const query = event.queryStringParameters.q
-
-        const searchResults = await client.search({
-            index: 'user',
-            body: {
-                query: {
-                    bool: {
-                        must: [
-                            {
-                                multi_match: {
-                                    query: query,
-                                    type: "phrase_prefix",
-                                    fields: ["firstName", "lastName", "fullName"]
-                                }
-                            },
-                        ],
-                        filter: [
-                            { "term": { "brokerageId": brokerageId } }
-                        ]
+                        }
                     }
+                })
+
+                const dbResults = searchResults.body.hits.hits.map(user => {
+                    const results = User.findOne({
+                        where: {
+                            id: user._source.id
+                        },
+                        include: {
+                            model: Team
+                        }
+                    })
+
+                    return results
+                })
+
+                const response = await Promise.all(dbResults)
+
+                return {
+                    body: JSON.stringify(response),
+                    headers: corsHeaders,
+                    statusCode: 200
                 }
+
             }
-        })
-
-        const dbResults = searchResults.body.hits.hits.map(user => {
-
-            const results = User.findOne({
-                where: {
-                    id: user._source.id
-                },
-                include: {
-                    model: Team
-                }
-            })
-
-            return results
-        })
-
-        const response = await Promise.all(dbResults)
-
-        return {
-            body: JSON.stringify(response),
-            headers: corsHeaders,
-            statusCode: 200
         }
-
     } catch (err) {
-
         console.log(err)
         return {
             headers: corsHeaders,
