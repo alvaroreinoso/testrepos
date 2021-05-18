fe28cae83883a860c152285dff1d09d4fe4c085b
Jerry Oates <jerryoates1@gmail.com>
2021-05-18 15:21:02 -0400

    remove log statements

--- helpers/mapbox.js
+++ helpers/mapbox.js
@@ -3,17 +3,13 @@ const fetch = require('node-fetch');
 
 module.exports.getLngLat = async (address) => {
     const resp = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${process.env.REACT_APP_MAPBOX_KEY}`)
-
     const respJson = await resp.json()
-
-    console.log(respJson)
 
     if (respJson.features.length == 0) {
         return null
     }
 
     const coords = respJson.features[0].geometry.coordinates
-
     const lnglat = await coords.toString()
 
     return lnglat
