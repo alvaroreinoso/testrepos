4f3bc779472050806400ca8045f092a03d194f3d
Jerry Oates <jerryoates1@gmail.com>
2021-05-27 16:20:07 -0400

    change opportunity to opportunities

--- helpers/showLaneOnMap.js
+++ helpers/showLaneOnMap.js
@@ -5,12 +5,12 @@ module.exports.showLaneOnMap = async(lane, status) => {
 	} if (lane.owned === false && status === 'owned') {
 		return false
 
-	} if (lane.owned === true && status === 'opportunity') {
+	} if (lane.owned === true && status === 'opportunities') {
 		return false
 
-	} if (lane.owned === false && status === 'opportunity') {
+	} if (lane.owned === false && status === 'opportunities') {
 		return true
-		
+
 	} if (status === 'potential') {
 		return true
 	}
